03b7e16eaa017b09724807685e842203836ae30b
Yaling327 <huangyl0723@gmail.com>
2021-08-22 00:36:34 +0900

    ログイン機能更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'pages#home'
   resources :diary_rubies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/users', to: 'users#index'
